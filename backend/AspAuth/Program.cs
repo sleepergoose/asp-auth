fd265f5e9731e7ed01e1f3341de168dbaa0b8e85
sleepergoose <sleepergoose@gmail.com>
2023-06-03 17:51:48 +0300

    Add initial settings

--- backend/AspAuth/Program.cs
+++ backend/AspAuth/Program.cs
@@ -1,6 +1,43 @@
+using DotNetEnv;
+using DotNetEnv.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+    .AddDotNetEnv(".env", LoadOptions.TraversePath())
+    .AddEnvironmentVariables()
+    .AddCommandLine(args)
+    .Build();
+
+builder.Configuration.AddConfiguration(config);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var origins = app.Configuration.GetSection("AllowedOrigins").Get<string[]>();
+
+app.UseCors(opt => opt
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins(origins!));
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
+
