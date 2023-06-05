991016cc42e8713acbff1e57abfaf9e37f952190
Ivan Slobodchykov <sleepergoose@gmail.com>
2023-06-05 21:16:43 +0300

    Add policies and claims (#3)

--- frontend/src/app/modules/main/components/home/home.component.ts
+++ frontend/src/app/modules/main/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { IClaim } from 'src/app/core/models/IClaim';
 import { AuthService } from 'src/app/core/services/auth.service';
 
@@ -12,11 +12,21 @@ import { AuthService } from 'src/app/core/services/auth.service';
 export class HomeComponent {
     claims$: Observable<IClaim[]> = this.authService.getClaims();
 
+    adminData: any;
+
     constructor(
         private readonly authService: AuthService,
         private readonly router: Router,
     ) {
 
+    }
+
+    getAdminData() {
+        this.authService.getAdminData()
+            .pipe(take(1))
+            .subscribe((data) => {
+                this.adminData = data;
+            });
     }
 
     logOut() {
