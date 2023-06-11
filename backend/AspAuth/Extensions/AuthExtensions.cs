0aab383900ef86f070f191a17952b05f5b82ec2b
sleepergoose <sleepergoose@gmail.com>
2023-06-11 18:37:49 +0300

    Refactor the code

---
+++ backend/AspAuth/Extensions/AuthExtensions.cs
@@ -0,0 +1,49 @@
+﻿using AspAuth.Enums;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace AspAuth.Extensions
+{
+    public static class AuthExtensions
+    {
+        public static AuthenticationBuilder AddAuthService(this IServiceCollection services)
+        {
+            return services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+                {
+                    options.Cookie.Name = "asp_auth_demo";
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                    options.SlidingExpiration = true;
+                    options.Events.OnRedirectToLogin = (context) =>
+                    {
+                        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                        return Task.CompletedTask;
+                    };
+                    options.Cookie.HttpOnly = true;
+                    // options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                });
+        }
+
+        public static IServiceCollection AddAuthorizationService(this IServiceCollection services)
+        {
+            return services.AddAuthorization(options =>
+            {
+                options.AddPolicy(Policies.Policies.Admin, policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.AddAuthenticationSchemes(CookieAuthenticationDefaults.AuthenticationScheme);
+                    policy.RequireClaim(Claims.Claims.Role, Role.Admin.ToString());
+                });
+
+                options.AddPolicy(Policies.Policies.User, policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.AddAuthenticationSchemes(CookieAuthenticationDefaults.AuthenticationScheme);
+                    policy.RequireClaim(Claims.Claims.Role, new[] { Role.Admin.ToString(), Role.User.ToString() });
+                });
+
+                options.DefaultPolicy = options.GetPolicy(Policies.Policies.User)!;
+            });
+        }
+    }
+}
---
+++ backend/AspAuth/Extensions/CorsExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace AspAuth.Extensions
+{
+    public static class CorsExtensions
+    {
+        public static IApplicationBuilder SetCors(this WebApplication app)
+        {
+            var origins = app.Configuration.GetSection("AllowedOrigins").Get<string[]>();
+
+            return app.UseCors(opt => opt
+                .AllowAnyHeader()
+                .WithMethods("GET", "POST", "UPDATE", "DELETE")
+                .AllowCredentials()
+                .SetIsOriginAllowed(origin =>
+                {
+                    if (string.IsNullOrWhiteSpace(origin))
+                        return false;
+
+                    if (origins is not null
+                        && origins.Any(o => o.ToLower().StartsWith(origin.ToLower())
+                            || o.ToLower().Contains(origin.ToLower())))
+                        return true;
+
+                    return false;
+                }));
+        }
+    }
+}
